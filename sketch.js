b32c093f0db17fa1a6b6ed7625404d0145a39dc9
isaacle77 <97554746+isaacle77@users.noreply.github.com>
2022-03-08 13:45:26 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(400, 400);
+
+    var block1_options = {
+		restitution : 0.5,
+		friction : 0.02,
+		frictionAir:0
+	}
+	 var block2_options = {
+		restitution : 0.7,
+		friction : 0.01,
+		frictionAir:0.1	
+	}
+	var block3_options = {
+		restitution : 0.01,
+		friction : 1,
+		frictionAir:0.3	
+	}
+
+	
+	
+    engine = Engine.create();
+	world = engine.world;
+
+	//Crie os Corpos Aqui.
+    block1 = Bodies.circle (220, 10, 10,block1_options); 
+	World.add(world, block1);
+
+    block2 = Bodies. rectangle (110, 50, 10, 10,block2_options); 
+	World.add(world, block2);
+
+    block3 = Bodies. rectangle (350, 50, 10, 10,block3_options); 
+	World.add(world, block3);
+
+   var plane_options ={
+	isStatic: true 
+   };
+	
+    plataforma = Bodies.rectangle(200, 390, 400, 10,plane_options);
+    World.add(world,plataforma);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  ellipseMode(RADIUS)
+  background("green");
+  Engine.update(engine);
+  drawSprites();
+  ellipse(block1.position.x,block1.position.y,10);
+  rect(block2.position.x,block2.position.y,10,10);
+  rect(block3.position.x,block3.position.y,10,10);
+  translate(plataforma.position.x,plataforma.position.y);
+}
+
+
+
